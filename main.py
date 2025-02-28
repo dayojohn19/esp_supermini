656ae6e3c4c57f707d176f8ddd93164f9ab818d8
dayojohn19 <dayo_john16@yahoo.com>
2025-03-01 01:12:24 +0800

    added ota

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 
 
 # for bare11
-from machine import Pin, I2C, RTC , DEEPSLEEP,SoftI2C
-
+from machine import Pin, I2C, RTC , DEEPSLEEP,SoftI2C, reset
 from configs.configs import sim_rx,sim_tx,sim_uart, wifiSSID ,wifipassword, groundPin, gatePin, doorPin,doorservoPin, ledsignalPin, BatteryMotorPin , feederpin
 from machine import UART, Pin, ADC, RTC, sleep, deepsleep,lightsleep, PWM, reset_cause
 from led_signal import LEDSignal
@@ -25,6 +24,130 @@ flyblink = 419
 ledlight = LEDSignal(ledsignalPin)
 ledlight.wink()
 time.sleep(2)
+
+
+
+
+
+
+
+import gc
+import os
+import json
+import urequests
+class OTAUpdater:
+    print("OTA Updating")
+    ledlight.start(100)
+    giturl = "https://github.com/dayojohn19/esp_supermini/"
+    files_to_update=["main.py"]
+    def __init__(self, repo_url=giturl, filenames=files_to_update):
+        self.filenames = filenames
+        self.repo_url = repo_url
+        if "www.github.com" in self.repo_url :
+            print(f"Updating {repo_url} to raw.githubusercontent")
+            self.repo_url = self.repo_url.replace("www.github","raw.githubusercontent")
+        elif "github.com" in self.repo_url:
+            print(f"Updating {repo_url} to raw.githubusercontent'")
+            self.repo_url = self.repo_url.replace("github","raw.githubusercontent")            
+        self.version_url = self.repo_url + 'main/version.json'
+        print(f"version url is: {self.version_url}")
+        if 'version.json' in os.listdir():    
+            gc.collect()
+            with open('version.json') as f:
+                self.current_version = int(json.load(f)['version'])
+                f.close()
+            print(f"Current device firmware version is '{self.current_version}'")
+        else:
+            self.current_version = 0
+            with open('version.json', 'w') as f:
+                json.dump({'version': self.current_version}, f)
+                f.close()
+            gc.collect()
+        self.download_and_install_update_if_available()
+        
+    def fetch_latest_code(self,firmware_url)->bool:
+        response = urequests.get(firmware_url,timeout=7)
+        if response.status_code == 200:
+            print(f'Fetched latest firmware code, status: {response.status_code}')
+            self.latest_code = response.text
+            return True
+        elif response.status_code == 404:
+            print(f'Firmware not found - {firmware_url}.')
+            pass
+
+    def update_no_reset(self):
+        with open('latest_code.py', 'w') as f:
+            f.write(self.latest_code)
+        self.current_version = self.latest_version
+        self.latest_code = None
+        return
+
+    def update_and_reset(self,filename):
+        os.rename('latest_code.py', filename)  
+        return
+
+    def check_for_updates(self):
+        """ Check if updates are available."""
+        response = urequests.get(self.version_url)
+        try:
+            data = json.loads(response.text)
+            self.latest_version = int(data['version'])
+            print(f'latest version is: {self.latest_version}')
+        except:
+            self.latest_version = 2
+            self.current_version = 1
+        newer_version_available = True if self.current_version < self.latest_version else False
+        print(f'Newer version available: {newer_version_available}')    
+        return newer_version_available
+    
+    def download_and_install_update_if_available(self):
+        gc.collect()
+        if self.check_for_updates():
+            print('Downloading latest code...')
+            self.firmware_urls = []
+            for filename in self.filenames:
+                time.sleep(0.1)
+                gc.collect()
+                url = self.repo_url + 'main/' + filename
+                self.firmware_urls.append(url)
+            for i in range(len(self.firmware_urls)):
+                gc.collect()
+                try:
+                    if self.fetch_latest_code(self.firmware_urls[i]):
+                        self.update_no_reset() 
+                        self.update_and_reset(self.filenames[i]) 
+                except:
+                    print('Passing: ',self.firmware_urls[i])
+                time.sleep(1)
+            with open('version.json', 'w') as f:
+                json.dump({'version': self.current_version}, f)
+            # print('Restarting device... 5')
+            # sleep(5)
+            print("\n\n         Applying Updates\n\n")
+            time.sleep(3)
+            reset()  
+        else:
+            ledlight.stop()
+            print('No new updates available.')
+            time.sleep(2)
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Power:
     def __init__(self,pin = groundPin):
         self.pin = Pin(pin,Pin.OUT)
